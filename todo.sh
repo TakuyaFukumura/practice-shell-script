d75ade9fd507e1159ede50dcc2e93bd45a450d05
takuya-fukumura <blackchoco1114@gmail.com>
2025-07-12 22:57:44 +0900

    ToDoリストプログラム追加

---
+++ todo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# TODOリストを管理するシェルスクリプト
+# 使用方法:
+#   ./todo.sh add タスク内容   # タスクを追加
+#   ./todo.sh list           # タスク一覧を表示
+#   ./todo.sh del 番号       # 指定番号のタスクを削除
+
+TODO_FILE="storage/todo.txt"
+
+case "$1" in
+  add)
+    shift # タスク内容にスペースが含まれていることを想定している
+    echo "$*" >> "$TODO_FILE"
+    echo "追加しました。内容:「$*」"
+    ;;
+  list)
+    if [ -f "$TODO_FILE" ]; then # ファイルの存在有無判定
+      nl -w2 -s'. ' "$TODO_FILE" # 幅2桁で行番号を付けてファイル内容を表示
+    else
+      echo "TODOはありません。"
+    fi
+    ;;
+  del)
+    if [ -z "$2" ]; then # 引数の空チェック
+      echo "削除する番号を指定してください。"
+      exit 1
+    fi
+    sed -i "${2}d" "$TODO_FILE"
+    echo "削除しました。"
+    ;;
+  *)
+    echo "使い方: $0 {add タスク内容 | list | del 番号}"
+    ;;
+esac
